0d3af35a95b8cb01a179f6d35de1bac2c418dc77
apoorva-1199 <apoorva.sharma@comprotechnologies.com>
2021-09-01 17:23:08 +0530

    commit chnages after creating apiResponse Object

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var postData;
-var productData;
 var buttonData = {
     'all': setPostData,
     'img': fetchRoboImage,
     'news': slideShow,
     'video': videoPlayer,
     'shop': setShopItems
+}
+var apiResponse = {
+    'postData': {},
+    'productData': {}
 }
 
 
@@ -14,8 +16,7 @@ function checkPassword() {
     var myInput = document.getElementById("input");
     var paswd = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
     if (myInput.value.match(paswd)) {
-        selectedElement('all');
-        setPostData();
+        loadContainerData('all');
         return true;
     } else {
         alert('Wrong...!')
@@ -41,113 +42,127 @@ function onIconClick(obj) {
 }
 
 
-function loadContainerData(id){
+function loadContainerData(id) {
     selectedElement(id);
-    clearViewContainer();
-    if(buttonData[id]){
-    buttonData[id].call();
+    if (buttonData[id]) {
+        clearViewContainer();
+        buttonData[id].call();
     }
 }
 
 function clearViewContainer() {
-    var divName = document.getElementById("view-container");
-    divName.innerHTML = "";
+    let divName = document.getElementById("view-container");
+    if (divName.innerHTML.trim().length > 0) {
+        divName.innerHTML = "";
+    }
+
 }
 
 //For fetching fake json  
 async function fetchPostData() {
-    if (postData === undefined) {
-        postData = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+    if (Object.keys(apiResponse['postData']).length === 0 && apiResponse['postData'].constructor === Object) {
+        apiResponse['postData'] = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
     }
-    return postData;
+    return apiResponse['postData'];
 }
 
 async function setPostData() {
-    document.getElementById("view-container").classList.remove('products');
-    document.getElementById("view-container").classList.remove('roboimg');
+    let divName = document.getElementById("view-container");
+    divName.classList.remove('products');
+    divName.classList.remove('roboimg');
     let posts = await fetchPostData();
+    console.log(posts);
     let output = "&lt;h2&gt;Lists of Posts&lt;/h2&gt;";
-    document.getElementById("view-container").innerHTML = output;
+    divName.innerHTML = output;
     var ul = document.createElement('ul');
     posts.forEach(function (post) {
         var li = document.createElement('li');
         li.textContent = post.title;
         ul.appendChild(li);
     });
-    document.getElementById("view-container").appendChild(ul);
+    divName.appendChild(ul);
 }
 
 
 //For fetching images
 function fetchRoboImage() {
-    document.getElementById("view-container").classList.add('roboimg');
-    document.getElementById("view-container").classList.remove('products');
+    let divName = document.getElementById("view-container");
+    divName.classList.add('roboimg');
+    divName.classList.remove('products');
+    const imgURL = "https://robohash.org/";
     for (let i = 1; i <= 12; i++) {
-        var img = new Image(200, 200);
+        var img = new Image();
         img.className = 'robo';
-        img.src = "https://robohash.org/" + i + ".png";
+        img.src = imgURL + i + ".png";
 
-        var src = document.getElementById("view-container");
+        var src = divName;
         src.appendChild(img);
     }
 }
 
 //For Slide show
 function slideShow() {
-    document.getElementById("view-container").classList.remove('products');
-    var divName = document.getElementById("view-container");
+
+    let divName = document.getElementById("view-container");
+    divName.classList.remove('products');
     for (counter = 1; counter <= 3; counter++) {
         var imagem = document.createElement("img");
         imagem.className = "slide";
         imagem.src = "img-" + counter + ".jpg";
         divName.appendChild(imagem);
     }
-    document.getElementsByClassName("slide")[0].className = 'slide showing';
-
+    let slide = document.querySelector('.slide');
+    if (slide !== null) {
+        slide.className = 'slide showing';
+    }
     //for slide show
-    var slides = document.querySelectorAll('.row-3 .slide');
-    var currentSlide = 0;
-    var slideInterval = setInterval(nextSlide, 2000);
+    let slides = document.querySelectorAll('.row-3 .slide');
+    let currentSlide = 0;
+    let slideInterval = setInterval(nextSlide, 2000);
     function nextSlide() {
         slides[currentSlide].className = 'slide';
         currentSlide = (currentSlide + 1) % slides.length;
         slides[currentSlide].className = 'slide showing';
     }
+
 }
+
 
 //for video
 function videoPlayer() {
-    document.getElementById("view-container").innerHTML = "<iframe title='YouTube video player' id='player'  type=\'text/html\' width='640' height='390' src='http://www.youtube.com/embed/W-Q7RMpINVo' frameborder='0' allowFullScreen></iframe>";
+    let divName = document.getElementById("view-container");
+    const vidSrc = "<iframe title='YouTube video player' id='player'  type=\'text/html\' width='640' height='390' src='http://www.youtube.com/embed/W-Q7RMpINVo' frameborder='0' allowFullScreen></iframe>"
+    divName.innerHTML = vidSrc;
 }
 
 //for shopping 
 async function getShopItems() {
-    if (productData === undefined) {
-        productData = await (await fetch("https://fakestoreapi.com/products")).json();
+    if (Object.keys(apiResponse['productData']).length === 0 && apiResponse['productData'].constructor === Object) {
+        apiResponse['productData'] = await (await fetch("https://fakestoreapi.com/products")).json();
     }
-    return productData;
+    return apiResponse['productData'];
 }
 
 async function setShopItems() {
-    document.getElementById("view-container").classList.add('products');
-    document.getElementById("view-container").classList.remove('roboimg');
+    let divName = document.getElementById("view-container");
+    divName.classList.add('products');
+    divName.classList.remove('roboimg');
     let output = '<h2>Lists of Products</h2>';
     let products = await getShopItems();
     document.getElementById("view-container").innerHTML = output;
     products.forEach(function (product) {
         var img = document.createElement('img');
         img.src = src = product.image;
-        document.getElementById("view-container").appendChild(img);
+        divName.appendChild(img);
     });
 
 }
 
 function selectedElement(id) {
-    var remove = document.getElementsByClassName("active");
-    if (remove.length != 0) {
-        remove[0].classList.remove('active');
+    let remove = document.querySelector(".active");
+    if (remove !== null) {
+        remove.classList.remove('active');
     }
-
     var el = document.getElementById(id);
     el.classList.add('active');
 }
